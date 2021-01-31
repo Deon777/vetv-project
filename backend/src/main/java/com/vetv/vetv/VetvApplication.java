76bef8fffb58d034c5548b8203ca4e92f31dc23a
Igor Deon <igordeon709@gmail.com>
2021-01-31 16:38:20 -0300

    Project created

---
+++ backend/src/main/java/com/vetv/vetv/VetvApplication.java
@@ -0,0 +1,13 @@
+package com.vetv.vetv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetvApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vetv/vetv/VetvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetv.vetv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
